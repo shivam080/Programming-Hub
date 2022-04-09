747242b69da2c852263524f8edc571046c40a27e
Sunil@github <40825078+Sunilnishad@users.noreply.github.com>
2022-04-09 18:27:27 +0530

    Add files via upload
    hashcode

---
+++ HashC.java
@@ -0,0 +1,14 @@
+
+
+public class HashC
+{
+	public static void main(String[] ar)
+
+	{
+          HashC h = new HashC();
+          HashC h1 = new HashC();
+          System.out.println(h.hashCode());
+          System.out.println(h.toString());
+          System.out.println(h1.hashCode());
+	}
+}
