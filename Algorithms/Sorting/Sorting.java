cf7ef8cf0f38fbf6cbc2430cb2d44266e6d223fd
Sunil@github <40825078+Sunilnishad@users.noreply.github.com>
2022-04-10 12:03:29 +0530

    Modified sorting class

--- Algorithms/Sorting/Sorting.java
+++ Algorithms/Sorting/Sorting.java
@@ -1,6 +1,7 @@
 
 import java.util.*;
 
+
  /***
  *Sorting number in ascending order using arrays class
  */
@@ -10,10 +11,19 @@ public class Sorting
 
 	{
 
-	 int arr[] = {2,54,23,66,33,14,55};
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter number of element you want to enter :");
+     int num = sc.nextInt();
+    System.out.println("Enter "+num + " elements :");
+     int[] arr = new int[num];
+     for(int i=0;i<num;i++)
+     {
+        arr[i]+=sc.nextInt();
+     }
 
 	 Arrays.sort(arr);
-
+     
+     System.out.println("Sorted element are below \n");
 	 for(int i=0;i<arr.length;i++)
       {
       	System.out.print(arr[i]+ " ");
